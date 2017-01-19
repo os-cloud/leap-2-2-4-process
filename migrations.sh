c056c232e1424691a248582ad9fb2cd97737f053
Kevin Carter <kevin.carter@rackspace.com>
2017-01-19 15:06:46 -0600

    renamed files
    Signed-off-by: Kevin Carter <kevin.carter@rackspace.com>

--- migrations.sh
+++ migrations.sh
@@ -77,6 +77,13 @@ fi
 ### Run the DB migrations
 
 ### Run the redeploy tasks
+# Forget about the old Juno neutron agent container in inventory.
+#  This is done to maximize uptime by leaving the old systems in
+#  place while the redeployment work is going on.
+SCRIPTS_PATH="/opt/leap42/openstack-ansible-${NEWTON_RELEASE}/scripts" \
+  MAIN_PATH="/opt/leap42/openstack-ansible-${NEWTON_RELEASE}" \
+    ${UPGRADE_UTILS}/neutron-container-forget.sh
+
 link_release "/opt/leap42/openstack-ansible-${NEWTON_RELEASE}"
 RUN_TASKS=()
 RUN_TASKS+=("${UPGRADE_UTILS}/db-stop.yml")
@@ -87,6 +94,6 @@ RUN_TASKS+=("lxc-containers-create.yml")
 RUN_TASKS+=("setup-infrastructure.yml")
 RUN_TASKS+=("${UPGRADE_UTILS}/db-force-upgrade.yml")
 RUN_TASKS+=("setup-openstack.yml")
-RUN_TASKS+=("${UPGRADE_UTILS}/rfc1034_1035-cleanup.yml")
+RUN_TASKS+=("${UPGRADE_UTILS}/post-redeploy-cleanup.yml")
 run_items "/opt/openstack-ansible"
 ### Run the redeploy tasks
